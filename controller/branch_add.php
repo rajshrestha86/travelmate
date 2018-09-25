e616768ab340e979588976ab25cbac72bb082d61
Sabin Shrestha <sabinshrestha15@outlook.com>
2018-09-25 10:22:23 +0545

    Staff Registration Done

--- controller/branch_add.php
+++ controller/branch_add.php
@@ -1,19 +1,14 @@
 <?php
 	include 'connection.php';
-
-	if($_POST){
-		
+	if($_POST){		
 		$name=$_POST['name'];
 		$address=$_POST['address'];
-		$contact=$_POST['contact'];
-
-
-		$sql="INSERT INTO branch(name,address,contact) VALUES($name,$address,$contact)";
+		$contact=$_POST['contact'];		
+		$sql="INSERT INTO branch (name, address, contact) VALUES ('$name', '$address', '$contact');";
 		$result=mysqli_query($conn,$sql);
 		if($result)
-			echo "Added new branch successfully";
-		
+			echo "Success";
+		else
+			echo $conn->connect_error;
 	}
-	
-
 ?>
--- staff_register.php
+++ staff_register.php
@@ -1,36 +1,84 @@
-
 <?php 
-    $title="Register Staff";
-    include 'header.php';
-    
-?>
-
+   $title="Staff Registration";
+   include 'header.php';
+   ?>
 <div id="wrapper">
-
-    <!-- Navigation -->
-
-    <?php require 'navigation.php';?>
-
-    <!-- Page Content -->
-    <div id="page-wrapper">
-        <div class="container-fluid">
-
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">Staff Management - Staff Registration </h1>
-                </div>
+<!-- Navigation -->
+<?php require 'navigation.php';?>
+<!-- Page Content -->
+<div id="page-wrapper">
+   <div class="container-fluid">
+      <div class="row">
+         <div class="col-lg-12">
+            <h1 class="page-header">Staff Management - Register </h1>
+         </div>
+      </div>
+      <div class="container">
+         <form method="POST" action="./controller/staff_add.php">
+            <div class="form-group row">
+               <label class="col-sm-2 col-form-label">First Name</label>
+               <div class="col-sm-5">
+                  <input type="text" class="form-control" id="input_branch_name" placeholder="Enter First Name here" name="stf_fname_in" required>
+               </div>
             </div>
+            <div class="form-group row">
+               <label class="col-sm-2 col-form-label">Last Name</label>
+               <div class="col-sm-5">
+                  <input type="text" class="form-control" id="input_branch_address" placeholder="Enter Last Name Here" name="stf_lname_in" required>
+               </div>
+            </div>
+            <div class="form-group row">
+               <label class="col-sm-2 col-form-label">branch</label>
+               <div class="col-sm-5">
+                  <select name="stf_branch_in" id="branch_input" class="form-control">
+                  <?php
+                                        include './controller/connection.php';
 
-            <!-- ... Your content goes here ... -->
-            <p>Here we create a form to register new staffs.</p>
 
-        </div>
-    </div>
 
+                                    $sql="SELECT * FROM branch";
+                                    $result=mysqli_query($conn,$sql);
+                                    $count = 1;
+                                    while($row = mysqli_fetch_assoc($result)) {
+
+                              ?>
+                              <option value="<?php echo $row['id']?>"><?php echo $row['name']?></option>
+
+                                    <?php 
+                                        $count=$count+1;
+                                    }
+                                    ?>
+                  </select>
+               </div>
+            </div>
+            
+            <div class="form-group row">
+               <label class="col-sm-2 col-form-label">username</label>
+               <div class="col-sm-5">
+                  <input type="text" class="form-control" id="input_branch_contact" placeholder="Enter username for login" name="stf_username_in" required>
+               </div>
+            </div>
+            <div class="form-group row">
+               <label class="col-sm-2 col-form-label">password</label>
+               <div class="col-sm-5">
+                  <input type="password" class="form-control" id="input_branch_contact" placeholder="Enter Password Here" name="stf_password_in" required>
+               </div>
+            </div>
+            <div class="form-group row">
+               <label class="col-sm-2 col-form-label">role</label>
+               <div class="col-sm-5">
+                  <label class="radio-inline"><input type="radio" name="stf_role_in" value="staff" checked>Staff</label>
+                  <label class="radio-inline"><input type="radio" name="stf_role_in" value="admin" >Admin</label>
+               </div>
+            </div>
+               <div class="form-group row">
+                  <div class="offset-sm-3 offset-md-3 col-sm-10">
+                     <button type="submit" class="btn btn-primary">Register</button>
+                  </div>
+               </div>
+         </form>
+         </div>
+      </div>
+   </div>
 </div>
-
-<?php include 'footer.php';?>
-
-
-
-
+<?php require 'footer.php';?>
